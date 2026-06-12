8b3a1e1e6eb1fec0cdda5e5822d6731473ebbe66
Kevin20180 <kevinlucas7910@gmail.com>
2026-06-12 18:44:39 -0400

    feat: adiciona constante MARCADOR_X e MARCADOR_O para gerenciar melhor marcador 'X' e 'O'

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ class Game {
 	constructor(marcadorElementos, vezElemento) {
 		this.marcadorElementos = marcadorElementos;
 		this.vezElemento = vezElemento;
-		this._vez = 0;
+		this._vez = X;
 	}
 
 	get vez() {
@@ -14,13 +14,13 @@ class Game {
 	}
 	set vez(a) {
 		this._vez = a;
-		this.vezElemento.innerText = this._vez === 0 ? 'Vez: X' : 'Vez: O';
+		this.vezElemento.innerText = a === MARCADOR_X ? 'Vez: X' : 'Vez: O';
 	}
 
 	// marcar X ou O
 	marcar(marcadorElemento) {
-		marcadorElemento.innerText = this.vez === 0 ? 'X' : 'O';
-		this.vez = Number(!this.vez);
+		marcadorElemento.innerText = this.vez;
+		this.vez = this.vez === MARCADOR_X ? MARCADOR_O : MARCADOR_X;
 	}
 
 	// tentar marcar X ou O, não irá marcar se já estiver marcado
@@ -30,58 +30,64 @@ class Game {
 	}
 
 	obterMapa() {
-		return this.marcadorElementos.map(e => e.innerText === 'O');
+		return this.marcadorElementos.map((e) => {
+			if(e.innerText.length === 0) return;
+			return e.innerText;
+		})
 	}
 }
 
 // inicializar jogo
 const game = new Game(
-	document.querySelectorAll('.marker'),
+	[...document.querySelectorAll('.marker')],
 	document.querySelector('.turn')
 );
 
+const MARCADOR_X = 'X';
+const MARCADOR_O = 'O';
+
 // mapa de possibilidades de ganhar jogo
 const MAPA_GANHADOR = [
-    [
-        1, 1, 1,
-        0, 0, 0,
-        0, 0, 0,
-    ],
-    [
-        0, 0, 0,
-        1, 1, 1,
-        0, 0, 0,
-    ],
-    [
-        0, 0, 0,
-        0, 0, 0,
-        1, 1, 1,
-    ],
-    [
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1,
-    ],
-    [
-        0, 0, 1,
-        0, 1, 0,
-        1, 0, 0,
-    ],
-    [
-        1, 0, 0,
-        1, 0, 0,
-        1, 0, 0,
-    ],
-    [
-        0, 1, 0,
-        0, 1, 0,
-        0, 1, 0,
-    ],
-    [
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-    ],
+	[
+		1, 1, 1,
+		0, 0, 0,
+		0, 0, 0,
+	],
+	[
+		0, 0, 0,
+		1, 1, 1,
+		0, 0, 0,
+	],
+	[
+		0, 0, 0,
+		0, 0, 0,
+		1, 1, 1,
+	],
+	[
+		1, 0, 0,
+		0, 1, 0,
+		0, 0, 1,
+	],
+	[
+		0, 0, 1,
+		0, 1, 0,
+		1, 0, 0,
+	],
+	[
+		1, 0, 0,
+		1, 0, 0,
+		1, 0, 0,
+	],
+	[
+		0, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+	],
+	[
+		0, 0, 1,
+		0, 0, 1,
+		0, 0, 1,
+	],
 ];
 
 for(const marcador of game.marcadorElementos) {
