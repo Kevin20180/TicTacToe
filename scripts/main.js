3bc1d9d1c3dee9ce1219bbef3dbc43cc9e9c85fa
Kevin20180 <kevinlucas7910@gmail.com>
2026-06-12 21:02:39 -0400

    feat: adiciona partida para gerenciar inicio e fim

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,14 @@ class Game {
 	marcadores;
 	vezesMarcado;
 	vezElemento;
+	partida;
 	_vez;
 
-	constructor(marcadores, vezElemento) {
+	constructor(marcadores, vezElemento, partida) {
 		this.marcadores = marcadores;
 		this.vezesMarcado = 0;
 		this.vezElemento = vezElemento;
+		this.partida = partida;
 		this._vez = MARCADOR_X;
 	}
 
@@ -25,12 +27,15 @@ class Game {
 		if(!marcador) return;
 
 		marcador.marcar(this.vez);
+		this.partida.mapaMarcadores[pos] = this.vez;
 		this.vez = this.vez === MARCADOR_X ? MARCADOR_O : MARCADOR_X;
 		this.vezesMarcado++;
 	}
 
 	// tentar marcar X ou O, não irá marcar se já estiver marcado
 	tenteMarcar(pos) {
+		if(!this.partida._estaIniciado) return;
+
 		const marcador = this.marcadores[pos];
 		if(!marcador || marcador.simbolo) return;
 
@@ -47,8 +52,8 @@ class Game {
 				for(let i in mapa) {
 					if(!mapa[i]) continue;
 
-					const marcador = this.marcadores[i];
-					if(marcador.simbolo === simbolo) correspondencias++;
+					let marcadorSimbolo = this.partida.mapaMarcadores[i];
+					if(marcadorSimbolo === simbolo) correspondencias++;
 				}
 
 				if(correspondencias >= 3) return simbolo;
@@ -71,16 +76,54 @@ class Marcador {
 
 	marcar(simbolo) {
 		this._simbolo = simbolo;
-		
 
 		if(simbolo === MARCADOR_X) {
 			this.elemento.innerText = '✖️';
 			this.elemento.classList.remove('marker_o');
 			this.elemento.classList.add('marker_x');
-		} else {
+		}
+		else if(simbolo === MARCADOR_O) {
 			this.elemento.innerText = '️⭕️';
 			this.elemento.classList.remove('marker_x');
 			this.elemento.classList.add('marker_o');
+		}
+		else {
+			this.elemento.innerText = '';
+			this.elemento.classList.remove('marker_x');
+			this.elemento.classList.remove('marker_o');
+		}
+	}
+}
+
+class Partida {
+	_estaIniciado;
+	game;
+	mapaMarcadores;
+
+	constructor(game) {
+		this.game = game;
+		this.mapaMarcadores = Array(9).fill(undefined);
+	}
+
+	get estaIniciado() {
+		return this._estaIniciado;
+	}
+
+	iniciar() {
+		if(this._estaIniciado) return;
+		this._estaIniciado = true;
+	}
+
+	parar() {
+		this._estaIniciado = false;
+	}
+
+	resetar() {
+		this.parar();
+		this.mapaMarcadores = this.mapaMarcadores.fill(undefined);
+
+		for(const marcador of this.game.marcadores) {
+			marcador.marcar(undefined);
 		}
 	}
 }
@@ -135,13 +178,28 @@ const MAPA_GANHADOR = [
 // inicializar jogo
 const game = new Game(
 	[...document.querySelectorAll('.marker')].map(m => new Marcador(m)),
-	document.querySelector('.turn')
+	document.querySelector('.turn'),
+	new Partida()
 );
+
+game.partida.game = game;
+game.partida.iniciar();
 
 for(let i in game.marcadores) {
 	const marcador = game.marcadores[i];
 
 	marcador.elemento.addEventListener('click', () => {
+		if(!game.partida.estaIniciado) return;
+
 		game.tenteMarcar(i);
+
+		const ganhador = game.obterGanhador();
+		if(!ganhador) return;
+		
+		game.partida.parar();
+
+		setTimeout(() => {
+			if(ganhador) alert("O ganhador dessa partida é o " + ganhador);
+		}, 500)
 	})
 }
